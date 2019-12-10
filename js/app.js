0f7c7ce326a04ea70d9838eba4fc0ec485f0e2c6
WiwiMa <55667543+WiwiMa@users.noreply.github.com>
2019-12-10 15:10:35 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -114,3 +114,73 @@ for (oneMenuSection of allMenuSections) {
         mobMenuOpener.classList.remove('hide');
     });
 }
+
+
+/* Create additional section */
+
+function createAddSection () {
+	let newSection = document.createElement('section');
+	newSection.setAttribute('id', 'section4');
+	newSection.setAttribute('data-nav', 'Section 4');
+
+	let newSecDiv = document.createElement('div');
+	newSecDiv.classList.add('landing__container');
+
+	let newSecH2 = document.createElement('h2');
+	newSecH2.textContent = "Section 4";
+
+	let newSecPFirst = document.createElement('p');
+	newSecPFirst.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.";
+
+	let newSecPSec = document.createElement('p');
+	newSecPSec.textContent = "Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non.";
+
+	newSecDiv.appendChild(newSecH2);
+	newSecDiv.appendChild(newSecPFirst);
+	newSecDiv.appendChild(newSecPSec);
+	newSection.appendChild(newSecDiv);
+
+	document.querySelector('main').appendChild(newSection);
+}
+
+
+function createAddNavItem () {
+    let newLiElement = document.createElement('li');
+    let newAElement = document.createElement('a');
+    newAElement.classList.add('menu__link');
+    newAElement.classList.add('section4');
+    newAElement.setAttribute('href', '#section4');
+    newAElement.textContent = "Section 4";
+
+    newLiElement.appendChild(newAElement);  
+    document.querySelector('#navbar__list').appendChild(newLiElement);
+}
+
+
+/* Re-organise active sections */
+
+var initialOffset = window.pageYOffset;
+var halfWay = window.innerHeight / 2;
+
+window.onscroll = function () {
+    var currentOffset = window.pageYOffset;
+    if (initialOffset > currentOffset) {
+        document.querySelector('.page__header').classList.remove('navbar__scroll');
+    } else {
+        document.querySelector('.page__header').classList.add('navbar__scroll');
+    }
+    initialOffset = currentOffset;
+    let incNewSections = document.querySelectorAll('section');
+    for (const sec of incNewSections) {
+        if (sec.getBoundingClientRect().top < halfWay && sec.getBoundingClientRect().top >= 0) {
+            var prevActNavItem = document.querySelector('.menu__link.active');
+            if (!prevActNavItem) {} else {
+                prevActNavItem.classList.remove('active');
+                };
+            var activeSectionId = sec.getAttribute('id');
+            console.log(activeSectionId);
+            var activeNavItem = document.querySelector('.menu__link.' + activeSectionId);
+            activeNavItem.classList.add('active');
+        }
+    }
+};
